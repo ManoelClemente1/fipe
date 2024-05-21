4c303a8e6a4bfe4f64935821e7eda9580735e8db
Manoel Clemente <manoelclemente9@outlook.com>
2024-05-21 23:52:48 +0100

    add marcas

--- src/main/java/com/api/fipe/controller/MarcasController.java
+++ src/main/java/com/api/fipe/controller/MarcasController.java
@@ -1,16 +1,32 @@
 package com.api.fipe.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.api.fipe.model.MarcaModel;
+import com.api.fipe.model.dto.MarcaDTO;
+import com.api.fipe.service.MarcaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/marcas")
 public class MarcasController {
 
+    @Autowired
+    private MarcaService marcaService;
 
     @PostMapping
-    public void criarMarca(String nome){
+    public void criarMarca(@RequestBody MarcaDTO marcaDTO){
+        marcaService.criarNovaMarca(marcaDTO.getNome(), marcaDTO.getIdCategoria());
+    }
 
+    @GetMapping
+    public List<MarcaModel> buscarMarcas(){
+        return marcaService.buscarListaDeMarcas();
+    }
+
+    @GetMapping("/{idCategoria}")
+    public List<MarcaModel> buscarMarcasPorCategoria(@PathVariable(value = "idCategoria") Long idCategoria){
+        return marcaService.buscarListaDeMarcasPorCategoria(idCategoria);
     }
 }
